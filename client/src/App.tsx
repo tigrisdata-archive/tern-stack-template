c211d9bcb636dd660afba9539c7059fe2135f176
Phil Leggetter <phil@leggetter.co.uk>
2023-04-26 12:43:42 +0100

    chore: updating React App page to TERN

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import IconExpress from "./icons/IconExpress";
+import IconReact from "./icons/IconReact";
+import IconNodeJs from "./icons/IconNodeJs";
+import IconTigris from "./icons/IconTigris";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-tern-letters">
+          <span>T</span>
+          <span>E</span>
+          <span>R</span>
+          <span>N</span>
+        </div>
+        <div className="App-tern-logos">
+          <IconTigris className="App-tern-logo" />
+          <IconExpress className="App-tern-logo" />
+          <IconReact className="App-tern-logo" />
+          <IconNodeJs className="App-tern-logo" />
+        </div>
+        <p>The TERN stack consists of:</p>
+        <ul className="App-tech-list">
+          <li>
+            <a href="https://www.tigrisdata.com?utm_source=github&utm_medium=github&utm_campaign=tern-template">
+              Tigris
+            </a>
+          </li>
+          <li>
+            <a href="https://expressjs.com/">Express.js</a>
+          </li>
+          <li>
+            <a href="https://react.dev/">React</a>
+          </li>
+          <li>
+            <a href="https://nodejs.org">Node.js</a>
+          </li>
+        </ul>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.tigrisdata.com/blog/tern-stack/?utm_source=template&utm_medium=template&utm_campaign=tern-template"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to TERN
         </a>
       </header>
     </div>
---
+++ client/src/icons/IconTigris.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+function IconTigris(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="199.06 197.16 219.88 220.67"
+      {...props}
+    >
+      <path
+        d="m385.5 343.57-5.64-12.39a31.278 31.278 0 0 0-16.07-15.8l-12.51-5.41 12.42-5.67a31.041 31.041 0 0 0 15.63-16.15l5.39-12.48 5.64 12.39a31.278 31.278 0 0 0 16.07 15.8l12.51 5.41-12.42 5.67A31.236 31.236 0 0 0 390.89 331l-5.39 12.57Z"
+        style={{
+          fill: "currentColor",
+        }}
+      />
+      <path
+        d="M295.54 296.69V241.9h44.65v-44.74H199.06v44.74h44v55.04c0 55.71 28.32 120.89 108.24 120.89v-43.4c-40.81-.25-55.75-32.59-55.75-77.74Z"
+        style={{
+          fill: "currentColor",
+        }}
+      />
+    </svg>
+  );
+}
+
+export default IconTigris;
